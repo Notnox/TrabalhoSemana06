1773629422540526a9931200d0824d35bc31a6cb
Notnox <llrricardo@gmail.com>
2021-01-22 11:50:25 -0300

    Ajustes em pesquisa.
    - Necessário informar algum valor para tentar pesquisar um processo.

--- app/src/Components/utils/Pesquisa/Pesquisa.js
+++ app/src/Components/utils/Pesquisa/Pesquisa.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import './Pesquisa.css'
 
 const ComponentsUtilsPesquisa = () => {
+
+    const history = useHistory();
+    const [pesquisa, setPesquisa] = useState('')
+
+    const onChange = (ev) => {
+        setPesquisa(ev.target.value)
+    }
+
+    const onClick = () => {
+        if (pesquisa.length === 0) {
+            return alert('É necessário informar um valor na barra de pesquisa.')
+        }
+
+        history.push('/processos')
+    }
+
     return (
         <div className='PagesIndex__pesquisa'>
-            <input placeholder='Pesquise por uma informação do processo' className='PagesIndex__Input' />
-            <a className='PagesIndex__lupa' href='/processos'>🔎</a>
+            <input
+                placeholder='Pesquise por uma informação do processo'
+                className='PagesIndex__Input'
+                onChange={onChange}
+            />
+            <a
+                className='PagesIndex__lupa'
+                onClick={onClick}
+            >🔎</a>
         </div>
     );
 }
